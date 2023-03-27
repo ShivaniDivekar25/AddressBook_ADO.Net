b8a09d7253d07fd5195f86aa16fe9ff27cf3a1e9
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-03-27 17:27:17 +0530

    Modified to Add New Contact To DataBase Using ADO.Net

---
+++ AddressBook_ADO.Net/AddressBook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_ADO.Net
+{
+    public class AddressBook
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AddressBook_Service";
+        public void AddNewContactInDataBase(AddressBookModel addressBookModel)
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            try
+            {
+                using (sqlConnection)
+                {
+                    sqlConnection.Open();
+                    SqlCommand sqlCommand = new SqlCommand("SpAddingNewData", sqlConnection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                    sqlCommand.Parameters.AddWithValue("@FirstName", addressBookModel.FirstName);
+                    sqlCommand.Parameters.AddWithValue("@LastName", addressBookModel.LastName);
+                    sqlCommand.Parameters.AddWithValue("@Address", addressBookModel.Address);
+                    sqlCommand.Parameters.AddWithValue("@City", addressBookModel.City);
+                    sqlCommand.Parameters.AddWithValue("@State", addressBookModel.State);
+                    sqlCommand.Parameters.AddWithValue("@Zip", addressBookModel.Zip);
+                    sqlCommand.Parameters.AddWithValue("@PhoneNumber", addressBookModel.PhoneNumber);
+                    sqlCommand.Parameters.AddWithValue("@Email", addressBookModel.Email);
+
+                    int result = sqlCommand.ExecuteNonQuery();      //Execute stored procedure
+                    sqlConnection.Close();
+                    if (result >= 1)
+                        Console.WriteLine("New contact added successfully.");
+                    else
+                        Console.WriteLine("Error while adding");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- AddressBook_ADO.Net/Program.cs
+++ AddressBook_ADO.Net/Program.cs
@@ -4,9 +4,34 @@ namespace AddressBook_ADO.Net
 {
     internal class Program
     {
+        //What is ADO.Net - It is framework. ADO.Net is used to connected between UI and SSMS database.
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book System Using ADO.Net");
+            AddressBook addressBook = new AddressBook();
+            AddressBookModel addressBookModel = new AddressBookModel()
+            {
+                FirstName = "Shivani",
+                LastName = "Divekar",
+                Address = "Dange Chauk",
+                City = "Pune",
+                State = "Maharastra",
+                Zip = 40035,
+                PhoneNumber = 8983922703,
+                Email = "Shivani@gmail.com"
+            };
+            Console.WriteLine("Enter an option");
+            Console.WriteLine("1:Add new contact");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    addressBook.AddNewContactInDataBase(addressBookModel);
+                    break;
+                default:
+                    Console.WriteLine("Enter correct option");
+                    break;
+            }
         }
     }
 }
